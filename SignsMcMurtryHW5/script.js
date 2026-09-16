d8a70ea62fd20b6773ea1b014c9bd2d46c4f1a83
TASM8 <taylorannmcmurtry@live.com>
2026-09-16 11:18:38 -0600

    Added homework stuff.

---
+++ SignsMcMurtryHW5/script.js
@@ -0,0 +1,20 @@
+
+    console.log("This is going to appear on my console");
+   
+    let name="Galaxy Quest"; //string text
+    let year="1999"; //number/integer
+    const isMovie=true; //bolean
+    console.log(name, year, isMovie);
+
+    let age="How many years ago did it come out?";
+    console.log(age)
+
+    console.log(2026-1999);
+
+    
+
+    let first="The alumi cast of a space opera television series have to play their roles as the real thing when an alien race needs their help.";
+    let second="However, they also have to defend both Earth and the alien race from a reptilian warlord."
+    console.log(first + " ", second);
+
+    
