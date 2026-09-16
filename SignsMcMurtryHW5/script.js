b955f0af2573e539c7bf9af239508757c1b3787d
TASM8 <taylorannmcmurtry@live.com>
2026-09-16 11:42:27 -0600

    Content, I also need to make comments.

--- SignsMcMurtryHW5/script.js
+++ SignsMcMurtryHW5/script.js
@@ -13,8 +13,28 @@
 
     
 
-    let first="The alumi cast of a space opera television series have to play their roles as the real thing when an alien race needs their help.";
-    let second="However, they also have to defend both Earth and the alien race from a reptilian warlord."
+    let first="The alumni cast of a space opera television series have to play their roles as the real thing when an alien race needs their help.";
+    let second="However, they also have to defend both Earth and the alien race from a reptilian warlord.";
     console.log(first + " ", second);
 
+    let third="One of the many reasons I love this movie, is that my dad and I would watch it all the time.";
+    let fourth="My family are big movie people, and I am not. Not at all, that was hard. But when my dad showed me Galaxy Quest for the first time, I loved it.";
+    console.log(third + " ", fourth);
+
+    let fifth="I also love how they are making fun of the Sci-Fi genre and also fans of it.";
+    let sixth="My favorite quote is, Never Give Up, Never Surrender! That was my senior quote in my yearbook and the quote I had on my cap."
+    console.log(fifth + " ", sixth);
+
+    let seventh="How many years have I been watching it?";
+    console.log(seventh);
+
+    console.log(2026-2008)
+
+    let eighth="I have watched it at least once a year for the past 18, and the movie is 102 minutes, so the amount of hours I have spent watching this movie is";
+    console.log(eighth);
+
+    console.log(18*102);
+
+    let nineth="That is also just the minimum I have watched it much more than that."
+    console.log(nineth);
     
